f65d438497fceaf2f98e163166409bdb65656105
JinryuNishiyama <jinryunishiyama@gmail.com>
2022-07-06 17:33:09 +0900

    EmailAuthenticationコントローラの作成

---
+++ nishiyama/src/app/Http/Controllers/DemoAppControllers/EmailAuthenticationController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\DemoAppControllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\DemoAppModels\EmailAuthentication;
+
+class EmailAuthenticationController extends Controller
+{
+    public function create()
+    {
+        return view('DemoAppViews/registration');
+    }
+
+    public function store (Request $request)
+    {
+        $email_authentication = EmailAuthentication::create();
+
+        $email_authentication->authentication_code = str_pad(random_int(0, 999999), 6, 0, STR_PAD_LEFT);
+        $email_authentication->email = $request->email;
+
+        $email_authentication->save();
+
+        return redirect()
+            ->route('email_authentications.create');
+    }
+}
